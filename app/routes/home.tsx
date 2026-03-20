93ad5b07c574a6100a72f9a1b48dffc7f4197f3c
koby <kobe.lauwers@student.howest.be>
2026-03-20 10:21:01 +0100

    refactored the code to work more like adress book tutorial example

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,13 @@
-import type { Route } from "./+types/home";
-
-export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: "Meals app" },
-    { name: "description", content: "Welcome to the meals app" },
-  ];
-}
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center text-gray-500">
-      <h2 className="text-3xl font-bold mb-4 text-gray-800 dark:text-gray-200">Welcome to the Recipe Book!</h2>
-      <p className="text-lg">👈 Select a recipe from the sidebar to view its details.</p>
-      <p className="mt-2">Or use the search bar to find something delicious.</p>
-    </div>
+    <p id="index-page">
+      This is a demo for React Router Recipes.
+      <br />
+      Check out{" "}
+      <a href="https://reactrouter.com">
+        the docs at reactrouter.com
+      </a>
+      .
+    </p>
   );
 }
